a9ff1e45edb7cfb652fdfad2023c558bc37eb6b5
Sanne Janssen <sanne.janssen@wunderkraut.com>
2013-01-18 20:00:01 +0100

    more theming

--- sites/all/themes/james/template.php
+++ sites/all/themes/james/template.php
@@ -36,6 +36,10 @@ function james_form_alter(&$form, &$form_state, $form_id) {
   // Step 2 of the checkout process: personal information
   if ($form_id == 'commerce_checkout_form_checkout') {
 
+    /* Remove address 2, change address 1 to just 'address' */
+    unset($form['customer_profile_billing']['commerce_customer_address']['und'][0]['street_block']['premise']);
+    $form['customer_profile_billing']['commerce_customer_address']['und'][0]['street_block']['thoroughfare']['#title'] = 'Adres';
+
     // In dutch because it's not translatable
     $form['buttons']['continue']['#value'] = 'Verder naar verzending';
     
@@ -58,6 +62,9 @@ function james_form_alter(&$form, &$form_state, $form_id) {
 
     /* Remove 'or' between cancel & continue */
     unset($form['buttons']['back']['#prefix']);
+
+    /* Make sure shipping 'verzending via post' is the default */
+    $form['commerce_shipping']['shipping_service']['#default_value'] = 'post';
   }
 
   // Step 4 of the checkout process: place your order
@@ -66,19 +73,26 @@ function james_form_alter(&$form, &$form_state, $form_id) {
     // In dutch because it's not translatable
     $form['buttons']['continue']['#value'] = 'Plaats uw bestelling';
     $form['buttons']['back']['#value'] = 'Terug naar verzending';
+
+    /* Remove 'or' between cancel & continue */
+    unset($form['buttons']['back']['#prefix']);
   }
 
   // Step 5 of the checkout process: confirmation message
   if ($form_id == 'commerce_checkout_form_complete') {
 
     /* TODO check code*/
+    // dpm($form);
 
-    $order_url = $form['checkout_completion_message']['message']['#markup'];
-    $message = "<p>Uw bestelling is geplaatst. <a href='$order_url'>U kan deze hier bekijken</a></p>";
 
-    global $base_root;
-    $message .= "<p><a href='$base_root'>Klik hier om terug te keren naar de startpagina</a></p>";
-    $form['checkout_completion_message']['message']['#markup'] = $message;
+    
+
+    // $order_url = $form['checkout_completion_message']['message']['#markup'];
+    // $message = "<p>Uw bestelling is geplaatst. <a href='$order_url'>U kan deze hier bekijken</a></p>";
+
+    // global $base_root;
+    // $message .= "<p><a href='$base_root'>Klik hier om terug te keren naar de startpagina</a></p>";
+    // $form['checkout_completion_message']['message']['#markup'] = $message;
 
     // dpm($form);
   }
@@ -234,180 +248,3 @@ function james_file_link($variables) {
   // return '<span class="file">' . $icon . ' ' . l($link_text, $url, $options) . '</span>';
   return '<span class="file">' . l($link_text, $url, $options) . '</span>';
 }
-
-
-function james_preprocess_views_view(&$vars){
-
-  
-
-  /*
-  $view = $vars['view'];
-  if ($view -> name = 'frontpage_view'){
-    drupal_add_js(drupal_get_path('theme', 'tbase') . '/js/jquery.cycle.all.js');
-    drupal_add_js(drupal_get_path('theme', 'tbase') . '/js/slideshow.js');
-  }
-  */
-}
-
-
-
-
-
-
-/*
- * Add body classes if certain regions have content.
-
-
-
-function bartik_preprocess_html(&$variables) {
-  if (!empty($variables['page']['featured'])) {
-    $variables['classes_array'][] = 'featured';
-  }
-
-  if (!empty($variables['page']['triptych_first'])
-    || !empty($variables['page']['triptych_middle'])
-    || !empty($variables['page']['triptych_last'])) {
-    $variables['classes_array'][] = 'triptych';
-  }
-
-  if (!empty($variables['page']['footer_firstcolumn'])
-    || !empty($variables['page']['footer_secondcolumn'])
-    || !empty($variables['page']['footer_thirdcolumn'])
-    || !empty($variables['page']['footer_fourthcolumn'])) {
-    $variables['classes_array'][] = 'footer-columns';
-  }
-
-  // Add conditional stylesheets for IE
-  drupal_add_css(path_to_theme() . '/css/ie.css', array('group' => CSS_THEME, 'browsers' => array('IE' => 'lte IE 7', '!IE' => FALSE), 'preprocess' => FALSE));
-  drupal_add_css(path_to_theme() . '/css/ie6.css', array('group' => CSS_THEME, 'browsers' => array('IE' => 'IE 6', '!IE' => FALSE), 'preprocess' => FALSE));
-}
-
-/**
- * Override or insert variables into the page template for HTML output.
-
-function bartik_process_html(&$variables) {
-  // Hook into color.module.
-  if (module_exists('color')) {
-    _color_html_alter($variables);
-  }
-}
-
-/**
- * Override or insert variables into the page template.
-
-function bartik_process_page(&$variables) {
-  // Hook into color.module.
-  if (module_exists('color')) {
-    _color_page_alter($variables);
-  }
-  // Always print the site name and slogan, but if they are toggled off, we'll
-  // just hide them visually.
-  $variables['hide_site_name']   = theme_get_setting('toggle_name') ? FALSE : TRUE;
-  $variables['hide_site_slogan'] = theme_get_setting('toggle_slogan') ? FALSE : TRUE;
-  if ($variables['hide_site_name']) {
-    // If toggle_name is FALSE, the site_name will be empty, so we rebuild it.
-    $variables['site_name'] = filter_xss_admin(variable_get('site_name', 'Drupal'));
-  }
-  if ($variables['hide_site_slogan']) {
-    // If toggle_site_slogan is FALSE, the site_slogan will be empty, so we rebuild it.
-    $variables['site_slogan'] = filter_xss_admin(variable_get('site_slogan', ''));
-  }
-  // Since the title and the shortcut link are both block level elements,
-  // positioning them next to each other is much simpler with a wrapper div.
-  if (!empty($variables['title_suffix']['add_or_remove_shortcut']) && $variables['title']) {
-    // Add a wrapper div using the title_prefix and title_suffix render elements.
-    $variables['title_prefix']['shortcut_wrapper'] = array(
-      '#markup' => '<div class="shortcut-wrapper clearfix">',
-      '#weight' => 100,
-    );
-    $variables['title_suffix']['shortcut_wrapper'] = array(
-      '#markup' => '</div>',
-      '#weight' => -99,
-    );
-    // Make sure the shortcut link is the first item in title_suffix.
-    $variables['title_suffix']['add_or_remove_shortcut']['#weight'] = -100;
-  }
-}
-
-/**
- * Implements hook_preprocess_maintenance_page().
-
-function bartik_preprocess_maintenance_page(&$variables) {
-  // By default, site_name is set to Drupal if no db connection is available
-  // or during site installation. Setting site_name to an empty string makes
-  // the site and update pages look cleaner.
-  // @see template_preprocess_maintenance_page
-  if (!$variables['db_is_active']) {
-    $variables['site_name'] = '';
-  }
-  drupal_add_css(drupal_get_path('theme', 'bartik') . '/css/maintenance-page.css');
-}
-
-/**
- * Override or insert variables into the maintenance page template.
-
-function bartik_process_maintenance_page(&$variables) {
-  // Always print the site name and slogan, but if they are toggled off, we'll
-  // just hide them visually.
-  $variables['hide_site_name']   = theme_get_setting('toggle_name') ? FALSE : TRUE;
-  $variables['hide_site_slogan'] = theme_get_setting('toggle_slogan') ? FALSE : TRUE;
-  if ($variables['hide_site_name']) {
-    // If toggle_name is FALSE, the site_name will be empty, so we rebuild it.
-    $variables['site_name'] = filter_xss_admin(variable_get('site_name', 'Drupal'));
-  }
-  if ($variables['hide_site_slogan']) {
-    // If toggle_site_slogan is FALSE, the site_slogan will be empty, so we rebuild it.
-    $variables['site_slogan'] = filter_xss_admin(variable_get('site_slogan', ''));
-  }
-}
-
-/**
- * Override or insert variables into the node template.
-
-function bartik_preprocess_node(&$variables) {
-  if ($variables['view_mode'] == 'full' && node_is_page($variables['node'])) {
-    $variables['classes_array'][] = 'node-full';
-  }
-}
-
-/**
- * Override or insert variables into the block template.
-
-function bartik_preprocess_block(&$variables) {
-  // In the header region visually hide block titles.
-  if ($variables['block']->region == 'header') {
-    $variables['title_attributes_array']['class'][] = 'element-invisible';
-  }
-}
-
-/**
- * Implements theme_menu_tree().
-
-function bartik_menu_tree($variables) {
-  return '<ul class="menu clearfix">' . $variables['tree'] . '</ul>';
-}
-
-/**
- * Implements theme_field__field_type().
-
-function bartik_field__taxonomy_term_reference($variables) {
-  $output = '';
-
-  // Render the label, if it's not hidden.
-  if (!$variables['label_hidden']) {
-    $output .= '<h3 class="field-label">' . $variables['label'] . ': </h3>';
-  }
-
-  // Render the items.
-  $output .= ($variables['element']['#label_display'] == 'inline') ? '<ul class="links inline">' : '<ul class="links">';
-  foreach ($variables['items'] as $delta => $item) {
-    $output .= '<li class="taxonomy-term-reference-' . $delta . '"' . $variables['item_attributes'][$delta] . '>' . drupal_render($item) . '</li>';
-  }
-  $output .= '</ul>';
-
-  // Render the top-level DIV.
-  $output = '<div class="' . $variables['classes'] . (!in_array('clearfix', $variables['classes_array']) ? ' clearfix' : '') . '"' . $variables['attributes'] .'>' . $output . '</div>';
-
-  return $output;
-}
-*/
